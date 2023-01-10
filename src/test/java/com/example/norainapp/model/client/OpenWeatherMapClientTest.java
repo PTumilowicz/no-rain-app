51d6b6429bae3b22696ffb98b05ce071ab98ea8a
Pawel <paweltumilowicz@gmail.com>
2023-01-10 11:47:23 +0100

    writing more tests

--- src/test/java/com/example/norainapp/model/client/OpenWeatherMapClientTest.java
+++ src/test/java/com/example/norainapp/model/client/OpenWeatherMapClientTest.java
@@ -1,10 +1,32 @@
 package com.example.norainapp.model.client;
 
+import com.example.norainapp.model.Weather;
+import com.example.norainapp.model.WeatherDescription;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class OpenWeatherMapClientTest {
-    OpenWeatherMapClient openWeatherMapClient = new OpenWeatherMapClient();
+    OpenWeatherMapClient openWeatherMapClient = mock(OpenWeatherMapClient.class);
+
+    @Test
+    void getWeatherReturnsWeather() {
+        //Given
+        Weather givenWeather = new Weather("Kraków", 10, LocalDateTime.now(), WeatherDescription.CLOUDY);
+        when(openWeatherMapClient.getWeather("Kraków")).thenReturn(givenWeather);
+
+        //When
+        Weather weather = openWeatherMapClient.getWeather("Kraków");
+        System.out.println(weather);
+
+        //Then
+        //assertThat(weather, is(notNullValue()));
+    }
+
     @Test
     void getWeatherCityNotFoundShouldNotBreakTheMethod() {
         //Then
